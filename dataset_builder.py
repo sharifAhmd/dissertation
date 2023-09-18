36f759ebb8e57c51c7b8a13c94ec1d7f914f8859
Sharif Ahamed <sharifahamed485@gmail.com>
2023-09-18 14:40:16 +0100

    updated dataset builder

--- dataset_builder.py
+++ dataset_builder.py
@@ -4,6 +4,12 @@ import pandas as pd
 TRAIN_DIR = "solar_flare_training_data"
 os.makedirs(TRAIN_DIR, exist_ok = True)
 
+def classify_flare(x):
+    if 1e-5 <= x <= 1e-4:
+        return 'M'
+    elif x > 1e-4:
+        return 'X'
+
 def split_data_in_train_and_test(df, col_name, window):
     # target value 
     #M flare - between 10^-5 and 10^-4
@@ -26,9 +32,12 @@ def split_data_in_train_and_test(df, col_name, window):
     X_train_df = pd.DataFrame(X_train_list, columns=['X_train_{}'.format(i) for i in range(window)])
     Y_train_df = pd.DataFrame({'Y_train': Y_train_list})
     
+    # Apply the function to create a new column
+    Y_train_df['Flare_Class'] = Y_train_df['Y_train'].apply(classify_flare)
+    
     X_train_df.to_csv(os.path.join(TRAIN_DIR, "X_Train.csv"), index = False)
     Y_train_df.to_csv(os.path.join(TRAIN_DIR, "Y_Train.csv"), index = False)
-    
+
 if __name__ == "__main__":
     print("Before running it keep it same as data source like sol_22_23_24_data_no_missing_v1.0.csv")
     print("Enter column name: (this should be xs or xl)")
@@ -36,4 +45,5 @@ if __name__ == "__main__":
     print("Enter window size:")
     window_size = int(input())
     df = pd.read_csv("sol_22_23_24_data_no_missing_v1.0.csv")
+    
     split_data_in_train_and_test(df, col_name, window_size)
